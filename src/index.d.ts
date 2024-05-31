8a0112a93900b24e25e18229d70a57a5ff2cad57
lanesky <shangkeyun@gmail.com>
2024-05-31 16:39:42 +0900

    bugfix: type declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,29 @@
-declare module 'audio-recorder-js' {
-    export {
-        AudioRecorder,
-        RecorderStates,
-        RecordingProperties
+// src/index.d.ts
+declare module '@lanesky/audio-recorder-js' {
+    export class AudioRecorder {
+      constructor(workletNodeProcessorUrl: string);
+      onCreateVisualizers: (analyserNode: AnalyserNode) => unknown[];
+      onRecordingLengthUpdated: (len: string) => void;
+      onRecordingStateChanged: (isRecording: boolean) => void;
+      onRecordingMaxLenReached: () => void;
+      onDownloadReady: (url: string) => void;
+      initContext(): void;
+      startRecording(): void;
+      stopRecording(): void;
+      isInitialized(): boolean;
+      cleanup(): void;
     }
-}
+  
+    export enum RecorderStates {
+      IDLE,
+      RECORDING,
+      PAUSED,
+      STOPPED
+    }
+  
+    export interface RecordingProperties {
+      sampleRate: number;
+      channels: number;
+      bitsPerSample: number;
+    }
+  }
